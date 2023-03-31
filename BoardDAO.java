e7523d0ae6c06cf7284fec374dfd13756d8f2cad
Seseok97 <112687040+Seseok97@users.noreply.github.com>
2023-03-31 18:09:10 +0900

    0331!

--- BoardDAO.java
+++ BoardDAO.java
@@ -3,6 +3,7 @@ package com.itwillbs.board.boardReview;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.naming.Context;
@@ -24,6 +25,17 @@ public class BoardDAO {
 		
 		return con;
 	} // getCon() method end
+	
+	public void closeDB() {
+		try {
+			if(rs != null)rs.close();
+			if(pstmt != null)pstmt.close();
+			if(con != null)con.close();
+		} catch (SQLException e) {
+			System.out.println("DAO: closeDB() Error!");
+			e.printStackTrace();
+		}
+	}// closeDB() method end
 	
 	public void insertBoard(BoardDTO dto) throws Exception{
 		con = getCon();
@@ -54,36 +66,47 @@ public class BoardDAO {
 		pstmt.setString(11, dto.getFile());
 		
 		pstmt.executeUpdate();		
+		System.out.println("글쓰기 완료!");
+		
+		closeDB();
+		
 	}//insertBoard method end
 	
 	public ArrayList getBoardList(int startRow, int pageSize) throws Exception {
 		ArrayList boardList = new ArrayList();
-		
-		con = getCon();
-		String sql = "select * from board_review limit ?,?";
-		PreparedStatement pstmt = con.prepareStatement(sql);
-		
-		pstmt.setInt(1, startRow -1);
-		pstmt.setInt(2, pageSize);
-		
-		ResultSet rs = pstmt.executeQuery();
-		while(rs.next()) {
-			BoardDTO dto = new BoardDTO();
-			dto.setBno(rs.getInt("bno"));
-			dto.setName(rs.getString("name"));
-			dto.setPass(rs.getString("pass"));
-			dto.setSubject(rs.getString("subject"));
-			dto.setContent(rs.getString("content"));
-			dto.setReadcount(rs.getInt("readcount"));
-			dto.setRe_ref(rs.getInt("re_ref"));
-			dto.setRe_lev(rs.getInt("re_lev"));
-			dto.setRe_seq(rs.getInt("re_seq"));
-			dto.setDate(rs.getDate("date"));
-			dto.setIp(rs.getString("ip"));
-			dto.setFile(rs.getString("file"));
+		try{
+			con = getCon();
+			String sql = "select * from board_review"
+					+ " order by re_ref desc, re_seq asc limit ?,?";
+			PreparedStatement pstmt = con.prepareStatement(sql);
 			
-			boardList.add(dto);
-		}//while end
+			pstmt.setInt(1, startRow -1);
+			pstmt.setInt(2, pageSize);
+			
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				BoardDTO dto = new BoardDTO();
+				dto.setBno(rs.getInt("bno"));
+				dto.setName(rs.getString("name"));
+				dto.setPass(rs.getString("pass"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setContent(rs.getString("content"));
+				dto.setReadcount(rs.getInt("readcount"));
+				dto.setRe_ref(rs.getInt("re_ref"));
+				dto.setRe_lev(rs.getInt("re_lev"));
+				dto.setRe_seq(rs.getInt("re_seq"));
+				dto.setDate(rs.getDate("date"));
+				dto.setIp(rs.getString("ip"));
+				dto.setFile(rs.getString("file"));
+				
+				boardList.add(dto);
+			}//while end
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			closeDB();
+		}
+		
 		return boardList;
 		
 	}// getBoardList() method end
@@ -100,9 +123,50 @@ public class BoardDAO {
 			}
 		}catch(Exception e){
 			e.printStackTrace();
+		}finally {
+			closeDB();
 		}
 		return result;
 	} // getBoardCount() method end
+	
+	public BoardDTO getBoard(int bno) {
+		BoardDTO dto = null;
+		try {
+			con = getCon();
+			sql="select * from board_review where bno=?";
+			pstmt.setInt(1, bno);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dto = new BoardDTO();
+				dto.setBno(rs.getInt(1));
+				dto.setName(rs.getString(2));
+				dto.setPass(rs.getString(3));
+				dto.setSubject(rs.getString(4));
+				dto.setContent(rs.getString(5));
+				dto.setReadcount(rs.getInt(6));
+				dto.setRe_ref(rs.getInt(7));
+				dto.setRe_lev(rs.getInt(8));
+				dto.setRe_seq(rs.getInt(9));
+				dto.setDate(rs.getDate(10));
+				dto.setIp(rs.getString(11));
+				dto.setFile(rs.getString(12));
+				
+				System.out.println("DAO: "+bno+"번 글, 정보저장 완료!");
+				
+			}// if end
+			
+			
+			}catch(Exception e) {
+				
+			}finally {
+			
+		}// t-c-f end
+		
+		
+		return dto;
+	}// getBoard() end
 	
 
 }// BoardDAO class end
